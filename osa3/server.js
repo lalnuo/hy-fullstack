38c091c76953aa876e8ad69f2e8bc22d853e002c
Lalli Nuorteva <lalli.nuorteva@zalando.fi>
2018-01-28 12:50:30 +0200

    Osa 3 jatkuu

--- osa3/server.js
+++ osa3/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const persons = [
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -21,6 +26,27 @@ const persons = [
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  persons = persons.filter(person => person.id !== req.params.id);
+  res.sendStatus(200);
+});
+
+app.post("/api/persons", (req, res) => {
+  const id = Math.floor(Math.random() * 100000);
+  const name = req.body.name;
+  const number = req.body.number;
+  if (!name) {
+    res.status(400).send("Name can not be empty");
+  } else if (!number) {
+    res.status(400).send("Number can not be empty");
+  } else if (persons.find(person => person.name === name)) {
+    res.status(400).send("Name must be unique");
+  } else {
+    persons.push({ name, number, id });
+    res.sendStatus(200);
+  }
 });
 
 app.get("/api/persons/:id", (req, res) => {
