6d61f24036ec7b9e54c27d0ff69db8d3c2ec9c44
Lalli Nuorteva <lalli.nuorteva@zalando.fi>
2018-02-12 23:28:16 +0200

    more testing

--- osa4/tests/server.test.js
+++ osa4/tests/server.test.js
@@ -1,12 +1,12 @@
 const supertest = require("supertest");
 const app = require("../server");
 const api = supertest(app);
+const Blog = require("../blog");
 
 test("notes are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("body", "body");
+  const response = await api.get("/api/blogs").expect(200);
+
+  expect(response.body.length).toBe(0);
 });
 
 test("notes are returned as json", async () => {
@@ -14,6 +14,33 @@ test("notes are returned as json", async () => {
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+describe("creating blogs", () => {
+  beforeEach(async () => {
+    await Blog.remove({});
+    console.log("everything removed");
+  });
+  test("adding a new blog", async () => {
+    const newBlog = {
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send({
+        title: "foo",
+        author: "bar",
+        url: "bar",
+        likes: 44
+      })
+      .expect(201);
+    const blogs = await Blog.find({});
+
+    expect(blogs.length).toBe(1);
+  });
 });
 
 afterAll(() => {
